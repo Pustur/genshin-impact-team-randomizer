8eac95a928325e69771517c002c5ef83cd163daf
Loris Bettazza <loris.bettazza@gmail.com>
2022-04-16 14:25:34 +0200

    :tada: Initial commit
    Using solidjs/templates/ts

---
+++ README.md
@@ -0,0 +1,3 @@
+## Genshin Impact Randomizer
+
+> A web app to randomize your Genshin Impact team based on the characters that you own
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default App;
