9284d86c7e2ce9d211bf936d2a406f635eba0623
Loris Bettazza <loris.bettazza@gmail.com>
2022-04-22 18:26:53 +0200

    :sparkles: Add DisplayCard for displaying team characters

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,6 +10,8 @@ interface ICard extends Pick<JSX.DOMAttributes<HTMLButtonElement>, 'onClick'> {
   character?: DeepReadonly<GenshinCharacter>;
 }
 
+type IDisplayCard = ICard & Required<Pick<ICard, 'character'>>;
+
 type IInteractiveCard = Required<ICard>;
 
 const EmptyCard: Component = () => {
@@ -19,6 +21,37 @@ const EmptyCard: Component = () => {
         <img class={styles.emptyImage} src="/img/icons/empty.svg" alt="" />
       </div>
       <div class={styles.name}>--</div>
+    </div>
+  );
+};
+
+const DisplayCard: Component<IDisplayCard> = props => {
+  return (
+    <div class={`${styles.card} ${styles.selected}`}>
+      <div
+        class={styles.imageHolder}
+        classList={{
+          [styles.fourStar]: props.character.stars === 4,
+          [styles.fiveStar]: props.character.stars === 5,
+          [styles.collab]: props.character.collab,
+        }}
+      >
+        <img
+          class={styles.characterImage}
+          src={`/img/characters/${slugify(props.character.fullName)}.png`}
+          alt=""
+        />
+      </div>
+      <div class={styles.elementsContainer}>
+        {props.character.elements.map(element => (
+          <img
+            class={styles.element}
+            src={`/img/elements/${element}.svg`}
+            alt=""
+          />
+        ))}
+      </div>
+      <div class={styles.name}>{props.character.fullName}</div>
     </div>
   );
 };
@@ -62,11 +95,10 @@ const InteractiveCard: Component<IInteractiveCard> = props => {
 
 const Card: Component<ICard> = props => {
   if (!props.character) return <EmptyCard />;
-  if (props.onClick) {
-    return (
-      <InteractiveCard onClick={props.onClick} character={props.character} />
-    );
-  }
+  if (!props.onClick) return <DisplayCard character={props.character} />;
+  return (
+    <InteractiveCard onClick={props.onClick} character={props.character} />
+  );
 };
 
 export { Card };
